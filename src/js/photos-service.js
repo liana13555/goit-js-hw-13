65c68f32301440d4ac9a6c03bbfb44dd7e969899
Liana Baryshova <barishovaliana@gmail.com>
2021-07-26 23:06:19 +0200

    Update file

--- src/js/photos-service.js
+++ src/js/photos-service.js
@@ -8,7 +8,7 @@ export default class PhotosApiService {
     constructor() {
         this.searchRequest = '';
         this.page = 1;
-        this.per_page = 4;
+        this.per_page = 40;
     }
 
     async fetchPhotos() {

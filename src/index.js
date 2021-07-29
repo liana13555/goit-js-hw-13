c3bdd6fcbfa3b791b476e75582e359cf4a5a5b54
Liana Baryshova <barishovaliana@gmail.com>
2021-07-29 15:03:49 +0200

    Add SimpleLightbox

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import PhotosApiService from './js/photos-service';
 import getRefs from './js/get-refs';
 import photoCard from './templates/photo-card.hbs';
 import Notiflix from "notiflix";
-// import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
+import '../node_modules/simplelightbox/src/simple-lightbox.scss';
 
 const refs = getRefs();
-
 const photosApiService = new PhotosApiService();
+const lightbox = new SimpleLightbox('.photo-card a');
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
@@ -22,12 +23,12 @@ async function onSearch(e) {
     if (photosApiService.query.trim() === '') {
         return;
     }
+
     try {
         const response = await photosApiService.fetchPhotos();
-        await appendHitsMarkup(response.hits);
-        console.log(response.totalHits);
-        console.log(photosApiService.per_page);
-
+        appendHitsMarkup(response.hits);
+        lightbox.refresh();
+        
            if (response.totalHits > photosApiService.per_page ) {
                getSuccessMessage(`"Hooray! We found ${response.totalHits} images."`);
                refs.loadMoreBtn.classList.remove('is-hidden');
@@ -44,8 +45,10 @@ async function onSearch(e) {
 
 async function onLoadMore() {
     try {
-        const response = await photosApiService.fetchPhotos();     
-        return await appendHitsMarkup(response.hits);
+        const response = await photosApiService.fetchPhotos();
+        lightbox.refresh();
+        return appendHitsMarkup(response.hits);
+        
     } catch (error) {
         console.log(error);
     }
@@ -71,10 +74,3 @@ function getInfoMessage(message) {
 function getSuccessMessage(message) {
    Notiflix.Notify.success(message);
 }
-
-// let lightbox = new SimpleLightbox('.gallery a');
-
-// fetchPhotos(hits => {
-//     appendHitsMarkup(hits);
-//     lightbox.refresh();
-// })
